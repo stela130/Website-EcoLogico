c6ab39ea9e769694443c89fd24814b2c1c1bfa38
stela130 <114312120+stela130@users.noreply.github.com>
2022-09-28 10:03:32 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@ function mostraMap(id){
 
 /*************************** CADASTRO.HTML - VALIDA INPUT VAZIO ***************************/
 
-/*VALIDAR SE OS IMPUT ESTÃO PREENCHIDOS*/
-function validaCampo(){
+/*VALIDAR SE OS IMPUT ESTÃO PREENCHIDOS E EMITE UM ALERTA E ALTERA O STYLE*/
+function valCmp(){
 	// verifica se o campo nome completo está vazio
 	if (document.getElementById("txt-nm").value==""){
 		alert ("Nome Completo não informado");
@@ -72,5 +72,41 @@ function validaCampo(){
 	alert("Cadastro realizado com sucesso!");
 	/*atualiza a página para limpar dos os campos*/
 	window.location.reload();
-
 }
+
+/*SE O CAMPO FOR PREENCHIDO VOLTA O STYLE NORMAL*/
+function rtnCmp(){
+	//verificar se o campo nome completo não está vazio 
+	if (document.getElementById("txt-nm").value!=""){
+		document.getElementById("ico-nm").style.color = "#317f54";
+		document.getElementById("txt-nm").style.border = "1px solid #317f54";
+	}
+	//verificar se o campo endereço não está vazio 
+	if (document.getElementById("txt-end").value!=""){
+		document.getElementById("ico-end").style.color = "#317f54";
+		document.getElementById("txt-end").style.border = "1px solid #317f54";
+	}	
+	//verificar se o campo telefone não está vazio 
+	if (document.getElementById("txt-tel").value!=""){
+		document.getElementById("ico-tel").style.color = "#317f54";
+		document.getElementById("txt-tel").style.border = "1px solid #317f54";
+	}	
+		//verificar se o campo e-mail não está vazio 
+	if (document.getElementById("txt-mail").value!=""){
+		document.getElementById("ico-mail").style.color = "#317f54";
+		document.getElementById("txt-mail").style.border = "1px solid #317f54";
+	}
+	//verificar se o campo login não está vazio 
+	if (document.getElementById("txt-lgn").value!=""){
+		document.getElementById("ico-lgn").style.color = "#317f54";
+		document.getElementById("txt-lgn").style.border = "1px solid #317f54";
+	}
+	//verificar se o campo senha não está vazio 
+	if (document.getElementById("txt-pwd").value!=""){
+		document.getElementById("ico-pwd").style.color = "#317f54";
+		document.getElementById("txt-pwd").style.border = "1px solid #317f54";
+	}
+}
+
+
+
