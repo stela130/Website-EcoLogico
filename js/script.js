1e8cebd2333798c65825da90ecff03a3f64ffaf1
stela130 <114312120+stela130@users.noreply.github.com>
2022-10-02 20:27:59 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ function mostraMap(id){
 	if (id == "mp-uir") {
 		document.getElementById("mp-uir").style.display = "block";
 		document.getElementById("mp-apa").style.display = "none";
-		document.getElementById("mp-jeq").style.display = "none";
+		document.getElementById("mp-suc").style.display = "none";
 	} 
 	if (id == "mp-apa") {
 		document.getElementById("mp-uir").style.display = "none";
 		document.getElementById("mp-apa").style.display = "block";
-		document.getElementById("mp-jeq").style.display = "none";
+		document.getElementById("mp-suc").style.display = "none";
 	}
-	if (id == "mp-jeq"){
+	if (id == "mp-suc"){
 		document.getElementById("mp-uir").style.display = "none";
 		document.getElementById("mp-apa").style.display = "none";
-		document.getElementById("mp-jeq").style.display = "block";
+		document.getElementById("mp-suc").style.display = "block";
 	}
 }
 
